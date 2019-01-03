19e3758266ee9bc10522f2c81cb6549e3ec6e248
JacoNeveS <jacoLPN@hotmail.com>
2019-01-03 06:09:13 +0000

    Primeiro commit, atualizando Git.

---
+++ JogoVelha/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace JogoVelha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Botoes);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(91, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Botoes);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(172, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Botoes);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(172, 41);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Botoes);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(91, 41);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Botoes);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(10, 41);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 3;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Botoes);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(172, 70);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 8;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Botoes);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(91, 70);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Botoes);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(10, 70);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 6;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Botoes);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(76, 108);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(101, 28);
+            this.btnLimpar.TabIndex = 9;
+            this.btnLimpar.Text = "ZERAR";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.BotaoLimpar);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 147);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ JogoVelha/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoVelha
+{
+    public partial class Form1 : Form
+    {
+        string[] textos = new string[9];
+        int rodadas = 0;
+        bool turno = true, fimDeJogo = false, IAJogou = false;
+        Random rdn = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void ChecarVencedor()
+        {
+            // Horizontais:
+            string Vazio = turno ? "X" : "O";
+
+            for (int i = 0; i <= 6; i += 3)
+            {
+                if (textos[i] == Vazio && textos[i] == textos[i +1] && textos[i] == textos[i +2])
+                {
+                    Vencedor();
+                }
+            }
+
+            // Verticais:
+            for (int i = 0; i <= 2; i++)
+            {
+                if (textos[i] == Vazio && textos[i] == textos[i +3] && textos[i] == textos[i + 6])
+                {
+                    Vencedor();
+                }
+            }
+            
+            // Diagonais:
+                //Diagonal Principal:
+            if (textos[0] == Vazio && textos[0] == textos[4] && textos[0] == textos[8])
+            {
+                Vencedor();
+            }
+
+                //Diagonal Secundária:
+            if (textos[2] == Vazio && textos[2] == textos[4] && textos[2] == textos[6])
+            {
+                Vencedor();
+            }
+
+            // Empate:
+            if (rodadas == 9 && !fimDeJogo)
+            {
+                fimDeJogo = true;
+                MessageBox.Show("Empate!");
+            }
+        }
+        void Vencedor()
+        {
+            fimDeJogo = true;
+            MessageBox.Show(String.Format("Jogador {0} venceu!", turno ? "X" : "O"));
+        }
+        private void Botoes(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (turno && !fimDeJogo && btn.Text == "")
+            {
+                btn.Text = "X";
+                textos[btn.TabIndex] = btn.Text;
+                rodadas++;
+                ChecarVencedor();
+                turno = !turno;
+                PensarIA();
+            }
+        }
+        private void Random()
+        {
+            bool encontrouLocal = false;
+
+            if (!turno)
+                while (!encontrouLocal)
+                {
+                    int sortear = rdn.Next(0, 9);
+
+                    foreach (var bt in Controls.OfType<Button>())
+                    {
+                        if (bt.Text == "" && bt.TabIndex == sortear)
+                        {
+                            // Encontrou um local válido para marcar no botão.
+                            encontrouLocal = true;
+                            IAJogar(sortear);
+                        }
+                    }
+                }
+        }
+        private void IAJogar (int tabindex)
+        {
+            foreach (var bt in Controls.OfType<Button>())
+            {
+                if (bt.TabIndex == tabindex && !turno && bt.Text == "")
+                {
+                    bt.Text = "O";
+                    textos[bt.TabIndex] = bt.Text;
+                    rodadas++;
+                    IAJogou = true;
+                    ChecarVencedor();
+                    turno = !turno;
+                }
+            }
+        }
+        private void IAValidar(string jogador)
+        {
+            // Checar horizontais:
+            // Variáveis locais de auxílio.
+            int iniciar = 0, limite = 0, verificarAoLado = 0, argumento = 0;
+
+            if (!turno && !fimDeJogo)
+            {
+                // Laço que representa as 3 formas de vencer
+                for (int i = 1; i <= 3; i++)
+                {
+                    // Validar o laço para modificar as variáveis auxiliares.
+                    switch (i)
+                    {
+                        case 1:
+                            limite = 6; verificarAoLado = 1; argumento = 2;
+                            break;
+                        case 2:
+                            verificarAoLado = 2; argumento = 1;
+                            break;
+                        case 3:
+                            iniciar = 1; limite = 7; verificarAoLado = 1; argumento = -1;
+                            break;
+                    }
+
+                    // Checar todas as 3 linhas das horizontais.
+                    for (int j = iniciar; j <= limite; j += 3)
+                    {
+                        if (textos[j] == jogador && textos[j] == textos[j + verificarAoLado])
+                        {
+                            /* Essa validação verifica se o conteúdo do vetor na posição do incremento do loop é igual ao parâmetro,
+                             * e se é igual ao do botão ao seu lado, e se o botão que deseja marcar está vazio, fazendo com que
+                             * o método para marcar a bolinha(o) seja chamado, passando como argumento o botão desejado. */
+                            IAJogar(j + argumento);
+                        }
+                    }
+                }
+            }
+
+            // Checar verticais:
+            for (int i = 1; i <= 3; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+                        iniciar = 0; limite = 2; verificarAoLado = 3; argumento = 6;
+                        break;
+                    case 2:
+                        verificarAoLado = 6; argumento = 3;
+                        break;
+                    case 3:
+                        iniciar = 3; limite = 5; verificarAoLado = 3; argumento = -3;
+                        break;
+                }
+
+                for (int j = iniciar; j <= limite; j++)
+                {
+                    if (textos[j] == jogador && textos[j] == textos[j + verificarAoLado])
+                    {
+                        IAJogar(j + argumento);
+                    }
+                }
+            }
+
+            //Checar diagonais:
+            for (int i = 1; i <= 6; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+                        iniciar = 0; verificarAoLado = 4; argumento = 8;
+                        break;
+                    case 2:
+                        verificarAoLado = 8; argumento = 4;
+                        break;
+                    case 3:
+                        iniciar = 4; verificarAoLado = 8; argumento = 0;
+                        break;
+                    case 4:
+                        iniciar = 2; verificarAoLado = 4; argumento = 6;
+                        break;
+                    case 5:
+                        verificarAoLado = 6; argumento = 4;
+                        break;
+                    case 6:
+                        iniciar = 4; verificarAoLado = 6; argumento = 2;
+                        break;
+                }
+
+                if (textos[iniciar] == jogador && textos[iniciar] == textos[verificarAoLado])
+                {
+                    IAJogar(argumento);
+                }
+            }
+        }
+        private void PensarIA()
+        {
+            IAJogou = false;
+
+            for (int i = 0; i <= 2; i++)
+            {
+                if (IAJogou)
+                {
+                    break;
+                }
+
+                if (!IAJogou && !fimDeJogo)
+                {
+                    switch (i)
+                    {
+                        //Vai tentar atacar.
+                        case 0:
+                            IAValidar("O");
+                            break;
+                        //Vai tentar defender.
+                        case 1:
+                            IAValidar("X");
+                            break;
+                        //Vai jogar aleatório.
+                        case 2:
+                            Random();
+                            break;
+                    }
+                }
+            }
+        }
+        private void BotaoLimpar(object sender, EventArgs e)
+        {
+            rodadas = 0;
+            fimDeJogo = false;
+            turno = !turno;
+            IAJogou = false;
+
+            foreach (var buttons in Controls.OfType<Button>())
+            {
+                buttons.Text = "";
+            }
+            btnLimpar.Text = "Recomeçar";
+
+            if (!turno)
+            {
+                PensarIA();
+            }
+        }
+
+    }
+}
